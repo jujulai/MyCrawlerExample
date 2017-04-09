3cbb9efce9af2a66e2f34a78e6d36712f9e0f04d
Ju <laitzuyu0401@outlook.com>
2017-04-09 16:05:07 +0800

    commit and push project testing

--- src/crawler/example/BasicExample.java
+++ src/crawler/example/BasicExample.java
@@ -2,6 +2,8 @@ package crawler.example;
 
 import com.github.abola.crawler.CrawlerPack;
 import org.apache.commons.logging.impl.SimpleLog;
+import org.jsoup.select.Elements;
+
 
 /**
  * 爬蟲包程式的全貌，就只有這固定的模式
@@ -20,23 +22,41 @@ public class BasicExample {
 		CrawlerPack.setLoggerLevel(SimpleLog.LOG_LEVEL_OFF);
 
 		// 遠端資料路徑
-		String uri = "http://.../";
+		String uri = "https://www.ptt.cc/bbs/Gossiping/M.1491708250.A.E18.html";
 
+/*
 		System.out.println(
+
 				CrawlerPack.start()
-				
+
 				// 參數設定
-			    //.addCookie("key","value")	// 設定cookie
+			    .addCookie("over18","1")	// 設定cookie
 				//.setRemoteEncoding("big5")// 設定遠端資料文件編碼
-				
 				// 選擇資料格式 (三選一)
-				.getFromJson(uri)
-			    //.getFromHtml(uri)
+				//.getFromJson(uri)
+			    .getFromHtml(uri)
 			    //.getFromXml(uri)
-			    
+
 			    // 這兒開始是 Jsoup Document 物件操作
-			    .select(".css .selector ")
-			    
+			    .select("div.push>span.f3")
+
 		);
+		*/
+
+		Elements jsoup = CrawlerPack.start()
+
+						// 參數設定
+						.addCookie("over18","1")	// 設定cookie
+						//.setRemoteEncoding("big5")// 設定遠端資料文件編碼
+						// 選擇資料格式 (三選一)
+						//.getFromJson(uri)
+						.getFromHtml(uri)
+						//.getFromXml(uri)
+
+						// 這兒開始是 Jsoup Document 物件操作
+						.select("#main-content.bbs-screen.bbs-content");
+		jsoup.select("div,span").remove();
+		System.out.println(jsoup.text());
+
 	}
 }
